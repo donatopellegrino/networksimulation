c837df0316ec49394cb49e7df239cb37002ed059
Daniele Del Sale <dainele.delsale@gmail.com>
2019-11-27 20:20:02 +0200

    BGP routing v0.5
    Still broken but it's an improvement

--- project-bgpd.cc
+++ project-bgpd.cc
@@ -19,86 +19,143 @@
  */
 
 #include "ns3/network-module.h"
+
 #include "ns3/core-module.h"
+
 #include "ns3/internet-module.h"
+
 #include "ns3/dce-module.h"
+
 #include "ns3/quagga-helper.h"
+
 #include "ns3/point-to-point-helper.h"
-#include "ns3/netanim-module.h"
+
+#include "ns3/applications-module.h"
+ //#include "ns3/netanim-module.h"
 
 using namespace ns3;
-NS_LOG_COMPONENT_DEFINE ("DceQuaggaBgpd");
+NS_LOG_COMPONENT_DEFINE("DceQuaggaBgpd");
 
 // Parameters
-uint32_t nNodes = 2;
-uint32_t stopTime = 1000;
+uint32_t nNodes = 3;
+uint32_t stopTime = 10000;
 
 
 
 
-int main (int argc, char *argv[])
-{
+int main(int argc, char * argv[]) {
 	//
 	//  Step 0
 	//  Node Basic Configuration
 	//
 
 	CommandLine cmd;
-	cmd.AddValue ("nNodes", "Number of Router nodes", nNodes);
-	cmd.AddValue ("stopTime", "Time to stop(seconds)", stopTime);
-	cmd.Parse (argc,argv);
+	cmd.AddValue("nNodes", "Number of Router nodes", nNodes);
+	cmd.AddValue("stopTime", "Time to stop(seconds)", stopTime);
+	cmd.Parse(argc, argv);
 
 	//
 	//  Step 1
 	//  Node Basic Configuration
 	//
 	NodeContainer nodes;
-	nodes.Create (nNodes);
+	nodes.Create(nNodes);
 
 	// Address conf In virtual topology
 	PointToPointHelper pointToPoint;
-	pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-	pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+	pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+	pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
 
-	NetDeviceContainer devices;
-	devices = pointToPoint.Install (nodes);
+
+	NetDeviceContainer ndc[2];
+	NodeContainer nc[2];
+	nc[0].Add(nodes.Get(0));
+	nc[0].Add(nodes.Get(1));
+	ndc[0] = pointToPoint.Install(nc[0]);
+	nc[1].Add(nodes.Get(1));
+	nc[1].Add(nodes.Get(2));
+	ndc[1] = pointToPoint.Install(nc[1]);
+
+
+
 
 
 	// Internet stack install
-	InternetStackHelper stack;    // IPv4 is required for GlobalRouteMan
-	Ipv4DceRoutingHelper ipv4RoutingHelper;	//required for quagga
-	stack.SetRoutingHelper (ipv4RoutingHelper);
-	stack.Install (nodes);
+	InternetStackHelper stack; // IPv4 is required for GlobalRouteMan
+	Ipv4DceRoutingHelper ipv4RoutingHelper; //required for quagga
+	stack.SetRoutingHelper(ipv4RoutingHelper);
+	stack.Install(nodes);
 
 	Ipv4AddressHelper ipv4AddrHelper;
-	ipv4AddrHelper.SetBase ("10.0.0.0", "255.255.255.0");
-	Ipv4InterfaceContainer interfaces = ipv4AddrHelper.Assign (devices);
-	Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-
+	ipv4AddrHelper.SetBase("10.0.0.0", "255.255.255.0");
+	Ipv4InterfaceContainer interfaces[2];
+	interfaces[0] = ipv4AddrHelper.Assign(ndc[0]);
+	ipv4AddrHelper.NewNetwork();
+	interfaces[1] = ipv4AddrHelper.Assign(ndc[1]);
+	Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+	
 	DceManagerHelper processManager;
-	processManager.SetNetworkStack ("ns3::Ns3SocketFdFactory");  // required for pcap
-	processManager.Install (nodes);
-	
+
+	processManager.SetNetworkStack("ns3::Ns3SocketFdFactory"); // required for pcap
+	processManager.Install(nodes);
 	QuaggaHelper quagga;
-	quagga.EnableBgp (nodes);
-	quagga.BgpAddNeighbor (nodes.Get (0), "10.0.0.2", quagga.GetAsn (nodes.Get (1)));
-	quagga.BgpAddNeighbor (nodes.Get (1), "10.0.0.1", quagga.GetAsn (nodes.Get (0)));
-	quagga.EnableZebraDebug (nodes);
-	quagga.Install (nodes);
+	quagga.EnableBgp(nodes);
+	quagga.BgpAddNeighbor(nc[0].Get(0), "10.0.0.2", quagga.GetAsn(nc[0].Get(1)));
+	quagga.BgpAddNeighbor(nc[0].Get(1), "10.0.0.1", quagga.GetAsn(nc[0].Get(0)));
+	quagga.BgpAddNeighbor(nc[1].Get(0), "10.0.1.2", quagga.GetAsn(nc[1].Get(1)));
+	quagga.BgpAddNeighbor(nc[1].Get(1), "10.0.1.1", quagga.GetAsn(nc[1].Get(0)));
+	//quagga.BgpAddPeerLink(nc[0].Get(0), "10.0.0.2");
+	//quagga.BgpAddPeerLink(nc[0].Get(1), "10.0.0.1");
+	//quagga.BgpAddPeerLink(nc[1].Get(0), "10.0.1.2");
+	//quagga.BgpAddPeerLink(nc[1].Get(1), "10.0.1.1");
+	//quagga.EnableZebraDebug(nodes);
+	quagga.Install(nodes);
+
+
+	pointToPoint.EnablePcapAll("project-bgpd");
 	
+	//uint32_t systemId = 0;
+	DceApplicationHelper dce;
+	ApplicationContainer apps;
 
+	dce.SetStackSize(1 << 20);
 
-	pointToPoint.EnablePcapAll ("project-bgpd");
+	// if ( systemId == nodes.Get(0)->GetSystemId () )
+	// {
+	// Launch ping on node 0
+	dce.SetBinary("ping");
+	dce.ResetArguments();
+	dce.ResetEnvironment();
+	dce.AddArgument("-c 10");
+	dce.AddArgument("-s 1000");
+	dce.AddArgument("10.0.1.2");
 
+	apps = dce.Install(nodes.Get(0));
+	apps.Start(Seconds(5.0));
+
+	// if ( systemId == nodes.Get(2)->GetSystemId ())
+	// {
+	//   // Launch ping on node 1
+	//   dce.SetBinary ("ping");
+	//   dce.ResetArguments ();
+	//   dce.ResetEnvironment ();
+	//   dce.AddArgument ("-c 10");
+	//   dce.AddArgument ("-s 1000");
+	//   dce.AddArgument ("10.0.0.1");
+
+	//   apps = dce.Install (nodes.Get(2));
+	//   apps.Start (Seconds (1.5));
+	// }
+	
 	//
 	// Now It's ready to GO!
 	//
-	Simulator::Stop (Seconds (stopTime));
-	AnimationInterface anim("myscripts/project/animation.xml");
-	anim.SetConstantPosition(nodes.Get(0), 10.0, 10.0);
-	anim.SetConstantPosition(nodes.Get(1), 30.0, 30.0);
-	Simulator::Run ();
-	Simulator::Destroy ();
+	Simulator::Stop(Seconds(stopTime));
+	//AnimationInterface anim("myscripts/project/animation.xml");
+	//anim.SetConstantPosition(nodes.Get(0), 10.0, 10.0);
+	//anim.SetConstantPosition(nodes.Get(1), 30.0, 30.0);
+	Simulator::Run();
+	Simulator::Destroy();
 
 	return 0;
 }
