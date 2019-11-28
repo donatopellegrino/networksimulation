45653abac0942eef069c2fc2cd61a700830bfe53
save196 <saverioturetta@gmail.com>
2019-11-28 12:54:19 +0200

    minor fix

--- project-bgpd.cc
+++ project-bgpd.cc
@@ -30,9 +30,8 @@ using namespace ns3;
 NS_LOG_COMPONENT_DEFINE ("DceQuaggaBgpd");
 
 // Parameters
-uint32_t nNodes = 3;
+uint32_t nNodes = 4;
 uint32_t stopTime = 1000;
-
 
 
 ApplicationContainer GenerateConfigBgp (Ptr<ns3::Node> node, int configuration);
@@ -58,11 +57,15 @@ int main (int argc, char *argv[])
     NodeContainer nodes1;
     // container for node 3
     NodeContainer nodes2;
-    
+    NodeContainer nodes3;
+
     nodes1.Add(nodes.Get(0));
     nodes1.Add(nodes.Get(1));
     nodes2.Add(nodes.Get(1));
     nodes2.Add(nodes.Get(2));
+    nodes3.Add(nodes.Get(2));
+    nodes3.Add(nodes.Get(3));
+
     // Address conf In virtual topology
     PointToPointHelper pointToPoint;
     pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
@@ -70,8 +73,11 @@ int main (int argc, char *argv[])
     
     NetDeviceContainer devices1;
     NetDeviceContainer devices2;
+    NetDeviceContainer devices3;
     devices1 = pointToPoint.Install (nodes1);
     devices2 = pointToPoint.Install (nodes2);
+    devices3 = pointToPoint.Install (nodes3);
+
     
     // Internet stack install
     InternetStackHelper stack;    // IPv4 is required for GlobalRouteMan
@@ -80,14 +86,18 @@ int main (int argc, char *argv[])
     stack.Install (nodes);
     
     Ipv4AddressHelper ipv4AddrHelper;
-    ipv4AddrHelper.SetBase ("10.0.0.0", "255.255.255.0");
+    ipv4AddrHelper.SetBase ("10.0.0.0", "255.255.255.252");
     Ipv4InterfaceContainer interfaces = ipv4AddrHelper.Assign (devices1);
     ipv4AddrHelper.NewNetwork();
     interfaces = ipv4AddrHelper.Assign (devices2);
+    ipv4AddrHelper.NewNetwork();
+    interfaces = ipv4AddrHelper.Assign (devices3);
+
     Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
     
     DceManagerHelper processManager;
     processManager.SetNetworkStack ("ns3::Ns3SocketFdFactory");  // required for pcap
+   
     processManager.Install (nodes);
     
 
@@ -106,16 +116,24 @@ int main (int argc, char *argv[])
     apps.Add(GenerateConfigBgp(nodes.Get(0),1));
     apps.Add(GenerateConfigBgp(nodes.Get(1),2));
     apps.Add(GenerateConfigBgp(nodes.Get(2),3));
-    
-    pointToPoint.EnablePcapAll ("project-bgpd");
+    apps.Add(GenerateConfigBgp(nodes.Get(3),4));
+
+
+    //RunIp (nodes.Get (0), Seconds (20.0), "bgp show");
+
+
+    Ptr<OutputStreamWrapper> routingStream = Create<OutputStreamWrapper> ("routes.log", std::ios::out);
+    ipv4RoutingHelper.PrintRoutingTableAllEvery (Seconds (20), routingStream);
+
+    //pointToPoint.EnablePcapAll ("project-bgpd");
     
     //
     // Now It's ready to GO!
     //
-    Simulator::Stop (Seconds (stopTime));
-    AnimationInterface anim("myscripts/project/animation.xml");
-    anim.SetConstantPosition(nodes.Get(0), 10.0, 10.0);
-    anim.SetConstantPosition(nodes.Get(1), 30.0, 30.0);
+    Simulator::Stop (Seconds (200.0));
+    //AnimationInterface anim("myscripts/project/animation.xml");
+    //anim.SetConstantPosition(nodes.Get(0), 10.0, 10.0);
+    //anim.SetConstantPosition(nodes.Get(1), 30.0, 30.0);
     Simulator::Run ();
     Simulator::Destroy ();
     
@@ -123,28 +141,59 @@ int main (int argc, char *argv[])
 }
 //Generating Config of BGP
 ApplicationContainer GenerateConfigBgp (Ptr<ns3::Node> node, int configuration)
-{
+{	
+
+
+
+
     // start the configuration of the application
     
      DceApplicationHelper process;
     ApplicationContainer apps;
-    /*
-    Ptr<QuaggaConfig> zebra_conf = node->GetObject<QuaggaConfig> ();
-    if (!zebra_conf)
-        {
-        zebra_conf = new QuaggaConfig ();
-        node->AggregateObject (zebra_conf);
-        }
-    GenerateConfigZebra (node);
+
+
+
+
+   
+
+
+  // config generation
+  std::stringstream conf_dir1, conf_file1;
+  // FIXME XXX
+  conf_dir1 << "files-" << node->GetId () << "";
+  ::mkdir (conf_dir1.str ().c_str (), S_IRWXU | S_IRWXG);
+  conf_dir1 << "/usr/";
+  ::mkdir (conf_dir1.str ().c_str (), S_IRWXU | S_IRWXG);
+  conf_dir1 << "/local/";
+  ::mkdir (conf_dir1.str ().c_str (), S_IRWXU | S_IRWXG);
+  conf_dir1 << "/etc/";
+  ::mkdir (conf_dir1.str ().c_str (), S_IRWXU | S_IRWXG);
+
+  conf_file1 << conf_dir1.str() << "/zebra.conf";
+  std::string zconfi;
+  zconfi ="hostname zebra \n"
+        "password zebra \n"
+        "log stdout\n"
+	"debug zebra kernel \n"
+	"debug zebra events \n"
+	"debug zebra packet \n";
+
+  std::ofstream conf1;
+  conf1.open (conf_file1.str ().c_str ());
+  conf1 <<zconfi; 
+  conf1.close ();
+
+
+  
     process.SetBinary ("zebra");
-    process.AddArguments ("-f", zebra_conf->GetFilename ());
+    process.AddArguments ("-f", "/usr/local/etc/zebra.conf");
     process.AddArguments ("-i", "/usr/local/etc/zebra.pid");
     process.SetStackSize (1 << 16);
     apps.Add (process.Install (node));
     apps.Get (0)->SetStartTime (Seconds (1.0 + 0.01 * node->GetId ()));
     node->AddApplication (apps.Get (0));
     //Ptr<BgpConfig> bgp_conf = node->GetObject<BgpConfig> ();
-     */
+     
     
     // config generation
     std::stringstream conf_dir, conf_file;
@@ -168,25 +217,63 @@ ApplicationContainer GenerateConfigBgp (Ptr<ns3::Node> node, int configuration)
     // passing the different configurations according to the node passed
     if (configuration == 1)
     {
-        confi ="log stdout \n"
+        confi ="hostname bgpd \n"
+	"password zebra \n"
+	"log stdout \n"
+	"debug bgp \n"
+	"debug bgp fsm \n"
+	"debug bgp events \n"
+	"debug bgp updates \n"
         "router bgp 1 \n"
-        "bgp router-id 10.0.0.1 \n"
-        "   neighbor 10.0.0.2 remote-as 1 \n";
+        "   bgp router-id 10.0.0.1 \n"
+        "   neighbor 10.0.0.2 remote-as 1 \n"
+	"! \n";
     }
     else if (configuration == 2)
     {
-        confi ="log stdout \n"
+        confi = "hostname bgpd \n"
+	"password zebra \n"
+	"log stdout \n"
+	"debug bgp \n"
+	"debug bgp fsm \n"
+	"debug bgp events \n"
+	"debug bgp updates \n"
         "router bgp 1 \n"
-        "bgp router-id 10.0.0.2 \n"
+        "   bgp router-id 10.0.0.2 \n"
         "   neighbor 10.0.0.1 remote-as 1 \n"
-        "   neighbor 10.0.1.2 remote-as 2 \n";
+	"   neighbor 10.0.0.1 next-hop-self \n"
+        "   neighbor 10.0.0.6 remote-as 2 \n"
+	"! \n";
     }
     else if (configuration == 3){
-        confi ="log stdout \n"
+        confi ="hostname bgpd \n"
+	"password zebra \n"
+	"log stdout \n"
+	"debug bgp \n"
+	"debug bgp fsm \n"
+	"debug bgp events \n"
+	"debug bgp updates \n"
         "router bgp 2 \n"
-        "bgp router-id 10.0.1.2 \n"
-        "   neighbor 10.0.1.1 remote-as 1 \n";
+        "   bgp router-id 10.0.1.2 \n"
+        "   neighbor 10.0.0.5 remote-as 1 \n"
+	"   neighbor 10.0.0.10 remote-as 2 \n"
+	"   neighbor 10.0.0.10 next-hop-self \n"
+	"! \n";
     }
+    else if (configuration == 4){
+        confi ="hostname bgpd \n"
+	"password zebra \n"
+	"log stdout \n"
+	"debug bgp \n"
+	"debug bgp fsm \n"
+	"debug bgp events \n"
+	"debug bgp updates \n"
+        "router bgp 2 \n"
+        "   bgp router-id 10.0.1.3 \n"
+        "   neighbor 10.0.0.9 remote-as 2 \n"
+	"! \n";
+    }
+
     //bgp_conf->Print (conf);
     conf<<confi;
     conf.close ();
